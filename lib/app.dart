29469a0ac884bf04b663d74c6d5eadf0ad47bbbd
Xurshidjon <161340754+Xurshidjondartdev@users.noreply.github.com>
2024-06-12 12:27:17 +0500

    app

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:pdp_quiz_app/core/route/go_route_systeam.dart';
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
